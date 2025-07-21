87c07f1f50fd8c40f896d8c5f73266ca7ac7e353
Piotr Seredynski <sersoft14@gmail.com>
2025-07-21 20:01:18 +0200

    Make a Circle method in class

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -1,13 +1,23 @@
 package core.basesyntax;
 
 public class Circle implements Figure{
+    private double radius;
+    private String color;
+
     @Override
     public double getArea() {
-        return 0;
+        return Math.PI * radius * radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
     }
 
     @Override
     public void draw() {
+        System.out.println("figure: circle, " + " area: " + getArea() + " sq. units, radius: "
+                + radius + " units, " + " color: " + color);
 
     }
 }
