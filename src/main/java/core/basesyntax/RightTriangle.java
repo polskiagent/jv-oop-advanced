209d480be96612bcc2ed90be48af3125794633df
Piotr Seredynski <sersoft14@gmail.com>
2025-07-21 20:23:48 +0200

    Make a RightTriangle method in class

--- src/main/java/core/basesyntax/RightTriangle.java
+++ src/main/java/core/basesyntax/RightTriangle.java
@@ -1,13 +1,23 @@
 package core.basesyntax;
 
 public class RightTriangle implements Figure{
+    private String color;
+    private double firstLeg;
+    private double secondLeg;
+
     @Override
     public double getArea() {
-        return 0;
+        return firstLeg * secondLeg /2;
+    }
+
+    public RightTriangle(double firstLeg, double secondLeg, String color) {
+        this.secondLeg = secondLeg;
+        this.firstLeg = firstLeg;
+        this.color = color;
     }
 
     @Override
     public void draw() {
-
+        System.out.println("figure: right triangle, " + " area: " + String.format("%.2f", getArea()) + " sq. units, " + " firstLeg: " + firstLeg + " units, " + " secondLeg: " + secondLeg + " units, " + " color: " + color);
     }
 }
