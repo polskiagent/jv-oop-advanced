dc4f3f42e32161bf7552eb171707ba0af5eace86
Piotr Seredynski <sersoft14@gmail.com>
2025-07-22 23:37:24 +0200

    Make a Trapezoid method in class

--- src/main/java/core/basesyntax/IsoscelesTrapezoid.java
+++ src/main/java/core/basesyntax/IsoscelesTrapezoid.java
@@ -1,13 +1,25 @@
 package core.basesyntax;
 
 public class IsoscelesTrapezoid implements Figure{
+    private String color;
+    private double topBase;
+    private double bottomBase;
+    private double height;
+
     @Override
     public double getArea() {
-        return 0;
+        return (topBase + bottomBase) * height /2;
+    }
+
+    public IsoscelesTrapezoid(String color, double topBase, double bottomBase, double height) {
+        this.color = color;
+        this.topBase = topBase;
+        this.bottomBase = bottomBase;
+        this.height = height;
     }
 
     @Override
     public void draw() {
-
+        System.out.println("figure: isosceles trapezoid, " + "area: " + String.format("%.2f", getArea()) + " sq. units, " + "topBase: " + topBase + " units, " + "bottomBase: " + bottomBase + " units, " + "height: " + height + " units, " + "color: " + color);
     }
 }
