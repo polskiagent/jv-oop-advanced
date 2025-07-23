66103a78980700f2db62250ac043c36fc2f586f8
Piotr Seredynski <sersoft14@gmail.com>
2025-07-23 06:44:05 +0200

    Make a Rectangle method in class

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,13 +1,24 @@
 package core.basesyntax;
 
 public class Rectangle implements Figure{
+    private String color;
+    private double width;
+    private double length;
+
     @Override
     public double getArea() {
-        return 0;
+        return width * length;
+    }
+
+    public Rectangle(String color, double length, double width) {
+        this.color = color;
+        this.width = width;
+        this.length = length;
     }
 
     @Override
     public void draw() {
+        System.out.println("figure: rectangle, " + "area: " + String.format("%.2f", getArea()) + " sq. units, " + "length: " + length + " units, " + "width: " + width + " units, " + "color: " + color);
 
     }
 }
