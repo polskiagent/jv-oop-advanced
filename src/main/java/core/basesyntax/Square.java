0f039277d1554f5222f74ba3bb650d758e060757
Piotr Seredynski <sersoft14@gmail.com>
2025-07-23 06:47:31 +0200

    Make a Square method in class

--- src/main/java/core/basesyntax/Square.java
+++ src/main/java/core/basesyntax/Square.java
@@ -1,13 +1,23 @@
 package core.basesyntax;
 
 public class Square implements Figure{
+    private String color;
+    private double side;
+
     @Override
     public double getArea() {
-        return 0;
+        return side * side;
+    }
+
+    public Square(double side, String color) {
+        this.side = side;
+        this.color = color;
     }
 
     @Override
     public void draw() {
+        System.out.println("figure: square, " + "area: " + String.format("%.2f", getArea()) + " sq. units, " + "side: " + side + " units, " + "color: " + color);
+
 
     }
 }
